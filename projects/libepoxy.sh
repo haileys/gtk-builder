ef1bb5af27052e11c3b862f050b427afdd840f3b
Hailey Somerville <hailey@hailey.lol>
2024-01-27 12:05:17 +1100

    add libepoxy

---
+++ projects/libepoxy.sh
@@ -0,0 +1,25 @@
+version="1.5.10"
+
+prepare() {
+    fetch-archive \
+        "https://github.com/anholt/libepoxy/archive/refs/tags/${version}.tar.gz" \
+        "a7ced37f4102b745ac86d6a70a9da399cc139ff168ba6b8002b4d8d43c900c15"
+}
+
+configure() {
+    cd "libepoxy-${version}"
+    meson setup \
+        --reconfigure \
+        --prefix="$PREFIX" \
+        -Ddefault_library=static \
+        -Dx11=false \
+        ../build
+}
+
+build() {
+    meson compile -C build
+}
+
+install() {
+    meson install -C build
+}
