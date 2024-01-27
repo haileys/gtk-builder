e8eeb08fbbccdee484c4eb0ae8e77a513491cc2b
Hailey Somerville <hailey@hailey.lol>
2024-01-27 12:29:31 +1100

    build glib from git submodule

--- projects/glib-common.sh
+++ projects/glib-common.sh
@@ -1,22 +1,25 @@
 [ -n "$GLIB_INTROSPECTION" ] || die "do not invoke glib-common.sh directly"
 
-version="2.79.1"
+version="git submodule"
 
 prepare() {
-    fetch-archive \
-        "https://download.gnome.org/sources/glib/2.79/glib-${version}.tar.xz" \
-        "b3764dd6e29b664085921dd4dd6ba2430fc19760ab6857ecfa3ebd4e8c1d114c"
+    # fetch-archive \
+    #     "https://download.gnome.org/sources/glib/2.79/glib-${version}.tar.xz" \
+    #     "b3764dd6e29b664085921dd4dd6ba2430fc19760ab6857ecfa3ebd4e8c1d114c"
+
+    true
 }
 
 configure() {
-    cd "glib-${version}"
+    cd "$BUILDER_ROOT/submodules/glib"
+
     meson subprojects update --reset
     meson setup \
         --reconfigure \
         --prefix="$PREFIX" \
         -Dintrospection="$GLIB_INTROSPECTION" \
         -Ddefault_library=static \
-        ../build
+        "$project_dir/build"
 }
 
 build() {
