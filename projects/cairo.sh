aa9e6047ce9e2ec9cf7541d9a4e5b6d07e3f0fd3
Hailey Somerville <hailey@hailey.lol>
2024-01-28 10:29:10 +1100

    cairo: disable lzo2 dep

--- projects/cairo.sh
+++ projects/cairo.sh
@@ -1,13 +1,14 @@
-version="1.18.0"
+version="git submodule"
 
 prepare() {
-    fetch-archive \
-        "https://gitlab.freedesktop.org/cairo/cairo/-/archive/${version}/cairo-${version}.tar.gz" \
-        "39a78afdc33a435c0f2ab53a5ec2a693c3c9b6d2ec9783ceecb2b94d54d942b0"
+    # fetch-archive \
+    #     "https://gitlab.freedesktop.org/cairo/cairo/-/archive/${version}/cairo-${version}.tar.gz" \
+    #     "39a78afdc33a435c0f2ab53a5ec2a693c3c9b6d2ec9783ceecb2b94d54d942b0"
+    true
 }
 
 configure() {
-    cd "cairo-${version}"
+    cd "$submodules/cairo"
 
     meson setup \
         --reconfigure \
@@ -15,7 +16,8 @@ configure() {
         -Ddefault_library=static \
         -Dfreetype=enabled \
         -Dtests=disabled \
-        ../build
+        -Dlzo2=disabled \
+        "$project_dir/build"
 }
 
 build() {
