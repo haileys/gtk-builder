d70f68605175ae3b1abf087178b325c8880467a7
Hailey Somerville <hailey@hailey.lol>
2024-01-30 10:12:42 +1100

    cmake: cross compile

--- lib/build/cmake.sh
+++ lib/build/cmake.sh
@@ -4,6 +4,22 @@ declare -a cmake_args
 cmake-project() {
     cmake_source_dir="$1"
 
+    # are we cross-compiling? set appropriate cmake args
+    if [ -n "${TARGET:-}" ]; then
+        local cross_sysroot
+        # ask gcc for sysroot
+        cross_sysroot="$("$TARGET-gcc" -print-sysroot)"
+        # normalise path
+        cross_sysroot="$(cd "$cross_sysroot" && pwd)"
+
+        cmake_args+=(
+            "-DCMAKE_C_COMPILER=$TARGET-gcc"
+            "-DCMAKE_CXX_COMPILER=$TARGET-g++"
+            "-DCMAKE_FIND_ROOT_PATH=$PREFIX;$cross_sysroot"
+            "-DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY"
+            "-DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY"
+        )
+    fi
 
     configure() {
         # cd and pwd to get absolute path to cmake source dir
