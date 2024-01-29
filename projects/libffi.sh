6d86a9da279977c77ebfb4791a486baf976dcc8d
Hailey Somerville <hailey@hailey.lol>
2024-01-30 10:13:20 +1100

    libffi: fix checksum

--- projects/libffi.sh
+++ projects/libffi.sh
@@ -3,7 +3,7 @@ version="3.4.4"
 prepare() {
     fetch-archive \
         "https://github.com/libffi/libffi/releases/download/v${version}/libffi-${version}.tar.gz" \
-        "-"
+        "d66c56ad259a82cf2a9dfc408b32bf5da52371500b84745f7fb8b645712df676"
 }
 
 configure() {
