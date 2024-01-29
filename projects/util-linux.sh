83a878e3a3cbb4ffe812fc8c7113553561251d65
Hailey Somerville <hailey@hailey.lol>
2024-01-30 10:44:22 +1100

    make util-linux a submodule

--- projects/util-linux.sh
+++ projects/util-linux.sh
@@ -1,13 +1,15 @@
-version="2.39.3"
+version="git submodule"
 
 prepare() {
-    fetch-archive \
-        "https://github.com/util-linux/util-linux/archive/refs/tags/v${version}.tar.gz" \
-        "2434edd1cf2aaca2a2b76b5de5ce7c98b12f75af9f600800c0655af20be85956" \
-        --filename "util-linux-${version}.tar.gz"
+    # fetch-archive \
+    #     "https://github.com/util-linux/util-linux/archive/refs/tags/v${version}.tar.gz" \
+    #     "2434edd1cf2aaca2a2b76b5de5ce7c98b12f75af9f600800c0655af20be85956" \
+    #     --filename "util-linux-${version}.tar.gz"
+
+    true
 }
 
-meson-project "util-linux-${version}"
+meson-project "$submodules/util-linux"
 
 meson_args+=(
     -Dbuild-chfn-chsh=disabled
