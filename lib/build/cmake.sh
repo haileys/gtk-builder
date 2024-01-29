ef7dc6127bd3a14aa042d04829d73695a8b3ec95
Hailey Somerville <hailey@hailey.lol>
2024-01-30 10:12:25 +1100

    cmake helper: can't normalise source dir until it exists

--- lib/build/cmake.sh
+++ lib/build/cmake.sh
@@ -2,19 +2,20 @@ cmake_source_dir=""
 declare -a cmake_args
 
 cmake-project() {
-    local source_dir="$1"
+    cmake_source_dir="$1"
 
-    # cd and pwd to get absolute path to cmake source dir
-    # the $(...) is a subshell so this will not have global effect
-    cmake_source_dir="$(cd "$project_dir" && cd "$source_dir" && pwd)"
 
     configure() {
+        # cd and pwd to get absolute path to cmake source dir
+        # the $(...) is a subshell so this will not have global effect
+        local source_dir="$(cd "$project_dir" && cd "$cmake_source_dir" && pwd)"
+
         cd build
 
         cmake \
             -DCMAKE_INSTALL_PREFIX="$PREFIX" \
             "${cmake_args[@]}" \
-            "$cmake_source_dir"
+            "$source_dir"
     }
 
     build() {
