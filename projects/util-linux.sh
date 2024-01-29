2b2a35f70b5688e912919f9c38151379e07b9b40
Hailey Somerville <hailey@hailey.lol>
2024-01-29 14:15:41 +1100

    build util-linux

---
+++ projects/util-linux.sh
@@ -0,0 +1,43 @@
+version="2.39.3"
+
+prepare() {
+    fetch-archive \
+        "https://github.com/util-linux/util-linux/archive/refs/tags/v${version}.tar.gz" \
+        "2434edd1cf2aaca2a2b76b5de5ce7c98b12f75af9f600800c0655af20be85956" \
+        --filename "util-linux-${version}.tar.gz"
+}
+
+configure() {
+    cd "util-linux-${version}"
+    meson setup \
+        --reconfigure \
+        --prefix="$PREFIX" \
+        -Ddefault_library=static \
+        -Dbuild-cramfs=disabled \
+        -Dbuild-fsck=disabled \
+        -Dbuild-libblkid=enabled \
+        -Dbuild-libfdisk=disabled \
+        -Dbuild-libmount=enabled \
+        -Dbuild-libsmartcols=disabled \
+        -Dbuild-libuuid=disabled \
+        -Dbuild-more=disabled \
+        -Dbuild-pg=disabled \
+        -Dbuild-python=disabled \
+        -Dbuild-setterm=disabled \
+        -Dbuild-ul=disabled \
+        -Dbuild-uuidd=disabled \
+        -Dncurses=disabled \
+        -Dncursesw=disabled \
+        -Dreadline=disabled \
+        -Dsystemd=disabled \
+        -Dtinfo=disabled \
+        ../build
+}
+
+build() {
+    meson compile -C build
+}
+
+install() {
+    meson install -C build
+}
