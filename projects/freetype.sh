4595d08590262cd47f2df7112378530f9f202fa1
Hailey Somerville <hailey@hailey.lol>
2024-01-30 16:06:11 +1100

    cross build freetype

--- projects/freetype.sh
+++ projects/freetype.sh
@@ -6,21 +6,9 @@ prepare() {
         "427201f5d5151670d05c1f5b45bef5dda1f2e7dd971ef54f0feaaa7ffd2ab90c"
 }
 
-configure() {
-    cd freetype-VER-2-13-2
-    meson setup \
-        --reconfigure \
-        --prefix="$PREFIX" \
-        -Ddefault_library=static \
-        -Dbzip2=disabled \
-        -Dbrotli=disabled \
-        ../build
-}
+meson-project "freetype-VER-2-13-2"
 
-build() {
-    meson compile -C build
-}
-
-install() {
-    meson install -C build
-}
+meson_args+=(
+    -Dbzip2=disabled
+    -Dbrotli=disabled
+)
