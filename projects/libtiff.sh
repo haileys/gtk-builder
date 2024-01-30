732ff34828512ff6f52fcd9c4a5579841239e044
Hailey Somerville <hailey@hailey.lol>
2024-01-30 15:55:51 +1100

    cross build libtiff and libxml2

--- projects/libtiff.sh
+++ projects/libtiff.sh
@@ -6,23 +6,13 @@ prepare() {
         "88b3979e6d5c7e32b50d7ec72fb15af724f6ab2cbf7e10880c360a77e4b5d99a"
 }
 
-configure() {
-    cd build
-    cmake \
-        -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-        -DBUILD_SHARED_LIBS=OFF \
-        -Dlzma=False \
-        -Dzstd=False \
-        -Dwebp=False \
-        -Djbig=False \
-        -Dlibdeflate=False \
-        "../tiff-${version}"
-}
+cmake-project "tiff-${version}"
 
-build() {
-    make -C build -j "$(nproc)"
-}
-
-install() {
-    make -C build install
-}
+cmake_args+=(
+    -DBUILD_SHARED_LIBS=OFF
+    -Dlzma=False
+    -Dzstd=False
+    -Dwebp=False
+    -Djbig=False
+    -Dlibdeflate=False
+)
