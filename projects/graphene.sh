5e19ec1d56129085104cc105f335a9124ba68c4b
Hailey Somerville <hailey@hailey.lol>
2024-01-30 16:47:58 +1100

    crossbuild graphene

--- projects/graphene.sh
+++ projects/graphene.sh
@@ -6,20 +6,8 @@ prepare() {
         "922dc109d2dc5dc56617a29bd716c79dd84db31721a8493a13a5f79109a4a4ed"
 }
 
-configure() {
-    cd "graphene-${version}"
-    meson setup \
-        --reconfigure \
-        --prefix="$PREFIX" \
-        -Ddefault_library=static \
-        -Dintrospection=disabled \
-        ../build
-}
+meson-project "graphene-${version}"
 
-build() {
-    meson compile -C build
-}
-
-install() {
-    meson install -C build
-}
+meson_args+=(
+    -Dintrospection=disabled
+)
