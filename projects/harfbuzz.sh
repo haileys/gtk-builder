5289e1f4925273ebe416333b948a05abd1095fd0
Hailey Somerville <hailey@hailey.lol>
2024-01-30 16:14:53 +1100

    fix harfbuzz dep order and cross build harfbuzz

--- projects/harfbuzz.sh
+++ projects/harfbuzz.sh
@@ -6,24 +6,13 @@ prepare() {
         "109501eaeb8bde3eadb25fab4164e993fbace29c3d775bcaa1c1e58e2f15f847"
 }
 
-configure() {
-    cd "harfbuzz-${version}"
-    meson setup \
-        --reconfigure \
-        --prefix="$PREFIX" \
-        -Ddefault_library=static \
-        -Dintrospection=disabled \
-        -Dtests=disabled \
-        -Dutilities=disabled \
-        -Dbenchmark=disabled \
-        -Ddocs=disabled \
-        ../build
-}
+meson-project "harfbuzz-${version}"
 
-build() {
-    meson compile -C build
-}
-
-install() {
-    meson install -C build
-}
+meson_args+=(
+    -Dbenchmark=disabled
+    -Ddocs=disabled
+    -Dicu=disabled
+    -Dintrospection=disabled
+    -Dtests=disabled
+    -Dutilities=disabled
+)
