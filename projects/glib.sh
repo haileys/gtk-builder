f5d673343b60acff3c171fb6283b3a60e932566b
Hailey Somerville <hailey@hailey.lol>
2024-01-30 14:55:05 +1100

    merge glib-common back into glib.sh

--- projects/glib.sh
+++ projects/glib.sh
@@ -1,2 +1,16 @@
-GLIB_INTROSPECTION=disabled
-source "$(dirname "${BASH_SOURCE[0]}")/glib-common.sh"
+version="git submodule"
+
+prepare() {
+    # fetch-archive \
+    #     "https://download.gnome.org/sources/glib/2.79/glib-${version}.tar.xz" \
+    #     "b3764dd6e29b664085921dd4dd6ba2430fc19760ab6857ecfa3ebd4e8c1d114c"
+
+    true
+}
+
+meson-project "$submodules/glib"
+
+meson_args+=(
+    -Dintrospection="$GLIB_INTROSPECTION"
+    -Dtests=false
+)
