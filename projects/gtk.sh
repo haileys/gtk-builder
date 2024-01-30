7bb5757b309affc63574c347844679991ca3d187
Hailey Somerville <hailey@hailey.lol>
2024-01-30 16:59:25 +1100

    crossbuild gtk

--- projects/gtk.sh
+++ projects/gtk.sh
@@ -5,27 +5,14 @@ prepare() {
     true
 }
 
-configure() {
-    cd "$submodules/gtk"
+meson-project "$submodules/gtk"
 
-    meson setup \
-        --reconfigure \
-        --prefix="$PREFIX" \
-        -Ddefault_library=static \
-        -Dintrospection=disabled \
-        -Dbuild-tests=false \
-        -Dbuild-testsuite=false \
-        -Dbuild-demos=true \
-        -Dbuild-examples=true \
-        -Dmedia-gstreamer=disabled \
-        -Dx11-backend=false \
-        "$project_dir/build"
-}
-
-build() {
-    meson compile -C build
-}
-
-install() {
-    meson install -C build
-}
+meson_args+=(
+    -Dintrospection=disabled
+    -Dbuild-tests=false
+    -Dbuild-testsuite=false
+    -Dbuild-demos=true
+    -Dbuild-examples=true
+    -Dmedia-gstreamer=disabled
+    -Dx11-backend=false
+)
