80957aefa6ef405e04577f83afe67b002612e240
Hailey Somerville <hailey@hailey.lol>
2024-02-13 18:47:07 +1100

    use released cairo version

--- projects/cairo.sh
+++ projects/cairo.sh
@@ -1,13 +1,12 @@
-version="git submodule"
+version="1.18.0"
 
 prepare() {
-    # fetch-archive \
-    #     "https://gitlab.freedesktop.org/cairo/cairo/-/archive/${version}/cairo-${version}.tar.gz" \
-    #     "39a78afdc33a435c0f2ab53a5ec2a693c3c9b6d2ec9783ceecb2b94d54d942b0"
-    true
+    fetch-archive \
+        "https://gitlab.freedesktop.org/cairo/cairo/-/archive/${version}/cairo-${version}.tar.gz" \
+        "39a78afdc33a435c0f2ab53a5ec2a693c3c9b6d2ec9783ceecb2b94d54d942b0"
 }
 
-meson-project "$submodules/cairo"
+meson-project "cairo-${version}"
 
 meson_args+=(
     -Dfreetype=enabled
