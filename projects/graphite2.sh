d87013e673e064ce5cd306566423ef20b039975a
Hailey Somerville <hailey@hailey.lol>
2024-02-13 18:42:55 +1100

    use graphite released version

--- projects/graphite2.sh
+++ projects/graphite2.sh
@@ -1,16 +1,13 @@
-version="git submodule"
+version="1.3.14"
 
 prepare() {
-    # fetch-archive \
-    #     "https://github.com/silnrsi/graphite/releases/download/${version}/graphite2-${version}.tgz" \
-    #     "f99d1c13aa5fa296898a181dff9b82fb25f6cc0933dbaa7a475d8109bd54209d"
-
-    true
+    fetch-archive \
+        "https://github.com/silnrsi/graphite/releases/download/${version}/graphite2-${version}.tgz" \
+        "f99d1c13aa5fa296898a181dff9b82fb25f6cc0933dbaa7a475d8109bd54209d"
 }
 
-cmake-project "$submodules/graphite2"
+cmake-project "graphite2-${version}"
 
 cmake_args+=(
-    -DBUILD_SHARED_LIBS=OFF
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
 )
