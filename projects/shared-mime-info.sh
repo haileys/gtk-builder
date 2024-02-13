a76ac3f95979da94f264ede5417c7d034d189f72
Hailey Somerville <hailey@hailey.lol>
2024-02-13 18:23:44 +1100

    shared-mime-info needs gettext bins

--- projects/shared-mime-info.sh
+++ projects/shared-mime-info.sh
@@ -1,5 +1,11 @@
 version="2.4"
 
+# on macOS put gettext bins in PATH just for this one build.
+# we never want to link to gettext
+if command -v brew >/dev/null; then
+    export PATH="$(brew --prefix gettext)/bin:$PATH"
+fi
+
 prepare() {
     fetch-archive \
         "https://gitlab.freedesktop.org/xdg/shared-mime-info/-/archive/${version}/shared-mime-info-${version}.tar.gz" \
