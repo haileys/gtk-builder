bc394d50dd524789ae7dec6b089bba4f78845c2c
Hailey Somerville <hailey@hailey.lol>
2024-02-13 18:29:01 +1100

    build gdk-pixbuf from released version

--- projects/gdk-pixbuf.sh
+++ projects/gdk-pixbuf.sh
@@ -1,14 +1,12 @@
-version="git submodule"
+version="2.42.10"
 
 prepare() {
-    # fetch-archive \
-    #     "https://download.gnome.org/sources/gdk-pixbuf/2.42/gdk-pixbuf-${version}.tar.xz" \
-    #     "ee9b6c75d13ba096907a2e3c6b27b61bcd17f5c7ebeab5a5b439d2f2e39fe44b"
-
-    true
+    fetch-archive \
+        "https://download.gnome.org/sources/gdk-pixbuf/2.42/gdk-pixbuf-${version}.tar.xz" \
+        "ee9b6c75d13ba096907a2e3c6b27b61bcd17f5c7ebeab5a5b439d2f2e39fe44b"
 }
 
-meson-project "$submodules/gdk-pixbuf"
+meson-project "gdk-pixbuf-${version}"
 
 meson_args+=(
     -Dbuiltin_loaders=all
