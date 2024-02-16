f9729066f1f387d08761cd47ae65b53223c05d9a
Hailey Somerville <hailey@hailey.lol>
2024-02-16 17:56:32 +1100

    add libadwaita

---
+++ projects/libadwaita.sh
@@ -0,0 +1,17 @@
+version="git submodule"
+
+prepare() {
+    # fetch-archive \
+    #     "https://download.gnome.org/sources/libadwaita/$(major-minor)/libadwaita-${version}.tar.xz" \
+    #     "ae9622222b0eb18e23675655ad2ba01741db4d8655a796f4cf077b093e2f5841"
+
+    true
+}
+
+meson-project "$submodules/libadwaita"
+
+meson_args+=(
+    -Dexamples=false
+    -Dtests=false
+    -Dintrospection=disabled
+)
