c6860f36e272e71a09a4dbe5804c5269210ff3e8
Hailey Somerville <hailey@hailey.lol>
2024-02-16 15:45:43 +1100

    add a new x86_64-macos-cross recipe

---
+++ recipes/x86_64-linux-static.sh
@@ -0,0 +1,54 @@
+recipe-init() {
+    declare -g -a autotools_args cmake_args
+
+    autotools_args+=(
+        --enable-static
+        --disable-shared
+    )
+
+    meson_args+=(
+        -Ddefault_library=static
+    )
+
+    cmake_args+=(
+        -DBUILD_STATIC_LIBS=ON
+        -DBUILD_SHARED_LIBS=OFF
+
+        # wish we could set this on a project-specific basis
+        -DPNG_STATIC=ON
+        -DPNG_SHARED=OFF
+
+        # for libjpeg-turbo
+        -DENABLE_STATIC=TRUE
+        -DENABLE_SHARED=FALSE
+    )
+}
+
+recipe-default-build() {
+    build-project zlib
+    build-project pcre2
+    build-project libffi
+    build-project util-linux
+    build-project glib
+    build-project libpng
+    build-project libjpeg-turbo
+    build-project libtiff
+    build-project libxml2
+    build-project shared-mime-info
+    build-project gdk-pixbuf
+    build-project freetype
+    build-project expat
+    build-project harfbuzz
+    build-project graphite2
+    build-project fontconfig
+    build-project pixman
+    build-project cairo
+    build-project fribidi
+    build-project pango
+    build-project graphene
+    build-project libepoxy
+    build-project wayland
+    build-project wayland-protocols
+    build-project xkbcommon
+    build-project gtk
+}
---
+++ recipes/x86_64-macos-cross.sh
@@ -0,0 +1,75 @@
+activate-brew-pkg() {
+    local pkg="$1"
+    local prefix
+    prefix="$(brew --prefix "$pkg")"
+    export PATH="$prefix/bin:$PATH"
+}
+
+recipe-init() {
+    export CC=clang
+    export CXX=clang++
+    export LD=clang
+
+    # export DYLD_LIBRARY_PATH="$TARGET_DIR/lib"
+    # export CFLAGS="-Wl,rpath,$TARGET_DIR/lib/ ${CFLAGS:-}"
+    # export LDFLAGS="-Wl,-rpath=@executable_path/../lib/"
+
+    # we might be running without much linked by homebrew into /usr/local,
+    # so put things we need in the path directly
+    activate-brew-pkg coreutils
+    activate-brew-pkg cmake
+    activate-brew-pkg ninja
+    # we don't activate meson this way bc it doesn't seem to work.
+    # brew link it
+
+    declare -g -a autotools_args cmake_args
+
+    autotools_args+=(
+        --enable-shared
+        --disable-static
+    )
+
+    meson_args+=(
+        -Ddefault_library=shared
+        --libdir="$TARGET_DIR/lib"
+    )
+
+    cmake_args+=(
+        -DBUILD_SHARED_LIBS=ON
+        # -DBUILD_STATIC_LIBS=OFF
+
+        "-DCMAKE_INSTALL_RPATH=@executable_path/../lib/"
+
+        # wish we could set this on a project-specific basis
+        -DPNG_SHARED=ON
+        -DPNG_STATIC=OFF
+
+        # for libjpeg-turbo
+        -DENABLE_SHARED=TRUE
+        -DENABLE_STATIC=FALSE
+    )
+}
+
+recipe-default-build() {
+    build-project pcre2
+    build-project libffi
+    build-project glib
+    build-project libpng
+    build-project libjpeg-turbo
+    build-project libtiff
+    build-project libxml2
+    build-project shared-mime-info
+    build-project gdk-pixbuf
+    build-project freetype
+    build-project expat
+    build-project harfbuzz
+    build-project graphite2
+    build-project pixman
+    build-project cairo
+    build-project fribidi
+    build-project pango
+    build-project graphene
+    build-project libepoxy
+    build-project gtk
+    build-project librsvg
+}
