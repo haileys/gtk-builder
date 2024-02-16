a0f0f83ddb234683352de2ccbf7e8e2bc3620962
Hailey Somerville <hailey@hailey.lol>
2024-02-16 16:03:56 +1100

    get cmake cross working

--- recipes/x86_64-macos-cross.sh
+++ recipes/x86_64-macos-cross.sh
@@ -1,42 +1,38 @@
-activate-brew-pkg() {
-    local pkg="$1"
-    local prefix
-    prefix="$(brew --prefix "$pkg")"
-    export PATH="$prefix/bin:$PATH"
-}
-
 recipe-init() {
-    export CC=clang
-    export CXX=clang++
-    export LD=clang
+    # some basic cross env checks
+    [ -n "${MACOS_KITS:-}" ] || die "MACOS_KITS not set, cannot proceed with cross build"
+    [ -n "${MACOS_SDK:-}" ] || die "MACOS_SDK not set, cannot proceed with cross build"
+    [ -d "$MACOS_KITS/osxcross/bin" ] || die "$MACOS_KITS/osxcross/bin not a directory, cannot proceed with cross build"
+    [ -e "$MACOS_SDK/usr/lib/libSystem.tbd" ] || die "MACOS_SDK does not appear to be a valid SDK sysroot"
 
-    # export DYLD_LIBRARY_PATH="$TARGET_DIR/lib"
-    # export CFLAGS="-Wl,rpath,$TARGET_DIR/lib/ ${CFLAGS:-}"
-    # export LDFLAGS="-Wl,-rpath=@executable_path/../lib/"
+    # activate the cross toolchain
+    export PATH="$MACOS_KITS/osxcross/bin:$PATH"
 
-    # we might be running without much linked by homebrew into /usr/local,
-    # so put things we need in the path directly
-    activate-brew-pkg coreutils
-    activate-brew-pkg cmake
-    activate-brew-pkg ninja
-    # we don't activate meson this way bc it doesn't seem to work.
-    # brew link it
+    export CC=o64-clang
+    export CXX=o64-clang++
+    export LD=o64-clang
 
-    declare -g -a autotools_args cmake_args
+    declare -g -a autotools_args cmake_args meson_args
 
+    # autotools config
     autotools_args+=(
         --enable-shared
         --disable-static
     )
 
+    # meson config
     meson_args+=(
         -Ddefault_library=shared
         --libdir="$TARGET_DIR/lib"
     )
 
+    # cmake config
+    local cmake_toolchain_cfg="$BUILD_DIR/cmake-toolchain.txt"
+    generate-cmake-cross-toolchain "$cmake_toolchain_cfg"
+
     cmake_args+=(
+        "-DCMAKE_TOOLCHAIN_FILE=$cmake_toolchain_cfg"
         -DBUILD_SHARED_LIBS=ON
-        # -DBUILD_STATIC_LIBS=OFF
 
         "-DCMAKE_INSTALL_RPATH=@executable_path/../lib/"
 
@@ -72,4 +68,31 @@ recipe-default-build() {
     build-project libepoxy
     build-project gtk
     build-project librsvg
+}
+
+generate-cmake-cross-toolchain() {
+    local file="$1"
+    local sysroot="$MACOS_SDK"
+
+    cat > "$file" <<END
+set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+set(CMAKE_SYSROOT "$sysroot")
+set(CMAKE_C_COMPILER o64-clang)
+set(CMAKE_CXX_COMPILER o64-clang++)
+set(CMAKE_LINKER x86_64-apple-darwin23-ld)
+set(CMAKE_AR x86_64-apple-darwin23-ar)
+set(CMAKE_NM x86_64-apple-darwin23-nm)
+set(CMAKE_RANLIB x86_64-apple-darwin23-ranlib)
+set(CMAKE_STRIP x86_64-apple-darwin23-strip)
+
+set(CMAKE_FIND_ROOT_PATH
+    "$TARGET_DIR"
+    "$sysroot")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+END
 }
