d41e08fe71706e664bc35b37f7292a7f26d199fd
Hailey Somerville <hailey@hailey.lol>
2024-02-19 13:24:12 +1100

    fix visited logic

--- lib/project/depends.sh
+++ lib/project/depends.sh
@@ -1,11 +1,18 @@
+declare -gA project_visited
+
 project::direct-depends() {
     project::with-env "$1" -- project::-write-depends
     cat "$BUILD_DIR/$1/.depends"
 }
 
 project::depends() {
-    declare -A project_visited
-    project::-depends-rec "$1"
+    # reset project_visited
+    project_visited=()
+
+    local project
+    for project in "$@"; do
+        project::-depends-rec "$project"
+    done
 }
 
 project::-depends-rec() {
@@ -15,10 +22,11 @@ project::-depends-rec() {
     project_visited["$project"]=1
 
     local depend
-    project::direct-depends "$project" | while read depend; do
+    for depend in $(project::direct-depends "$project"); do
         project::-depends-rec "$depend"
-        echo "$depend"
     done
+
+    echo "$project"
 }
 
 project::-write-depends() {
